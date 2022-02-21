aa04c7d57fd896cd4931fd5804707ccb14cc81b7
dongsu <sonicce99@naver.com>
2022-02-22 01:27:42 +0900

    feat : new Route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import { BrowserRouter } from "react-router-dom";
+import Main from "./components/Main";
 import Search from "./components/Search";
 import Compare from "./components/Compare";
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Search />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/compare" element={<Compare />} />
       </Routes>
     </BrowserRouter>
