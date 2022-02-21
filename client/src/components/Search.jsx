b796a4895bc5c89d88654bef1a5b27e1591406ec
dongsu <sonicce99@naver.com>
2022-02-22 00:34:49 +0900

    feat : Search component ui development

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   InputGroup,
@@ -7,49 +7,103 @@ import {
   DropdownButton,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 import styled from "styled-components";
+import { BiSearch } from "react-icons/bi";
+import { GrLocation } from "react-icons/gr";
+import { BsCheckCircle } from "react-icons/bs";
 
 const Search = () => {
+  const local1 = ["서울", "인천", "대구", "대전", "세종", "경남"];
+  const local2 = ["경기", "부산", "광주", "울산", "강원", "경북"];
+
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState("");
+
+  const handleTrue = () => setShow(true);
+  const handleFalse = () => setShow(false);
+  const handleValue = (e) => setValue(e.target.value);
+  const handleValueNull = () => setValue("");
+
   return (
-    <>
+    <Container>
       <InputGroup className="mb-3">
+        <BiSearch className="SearchIcon" />
         <FormControl
           placeholder="검색어를 입력하세요"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
+          value={value}
+          onChange={handleValue}
         />
+        <GrLocation className="LocationIcon" />
         <DropdownButton
           variant="outline-secondary"
           title="지역을 선택해주세요"
           id="input-group-dropdown-1"
         >
-          <Box>
+          <Box style={{ width: 410, height: 220 }}>
             <Div>
-              <Dropdown.Item href="#">서울</Dropdown.Item>
-              <Dropdown.Item href="#">인천</Dropdown.Item>
-              <Dropdown.Item href="#">대구</Dropdown.Item>
-              <Dropdown.Item href="#">대전</Dropdown.Item>
-              <Dropdown.Item href="#">세종</Dropdown.Item>
-              <Dropdown.Item href="#">경남</Dropdown.Item>
+              {local1.map((local, index) => {
+                return (
+                  <Box key={index}>
+                    <Dropdown.Item
+                      href="#"
+                      style={{
+                        width: 202,
+                        height: 34,
+                      }}
+                    >
+                      {local}
+                    </Dropdown.Item>
+                    <BsCheckCircle className="CheckIcon" />
+                  </Box>
+                );
+              })}
             </Div>
-
             <Div>
-              <Dropdown.Item href="#">경기</Dropdown.Item>
-              <Dropdown.Item href="#">부산</Dropdown.Item>
-              <Dropdown.Item href="#">광주</Dropdown.Item>
-              <Dropdown.Item href="#">울산</Dropdown.Item>
-              <Dropdown.Item href="#">강원</Dropdown.Item>
-              <Dropdown.Item href="#">경북</Dropdown.Item>
+              {local2.map((local, index) => {
+                return (
+                  <Box key={index}>
+                    <Dropdown.Item
+                      href="#"
+                      style={{
+                        width: 202,
+                        height: 34,
+                      }}
+                    >
+                      {local}
+                    </Dropdown.Item>
+                    <BsCheckCircle className="CheckIcon" />
+                  </Box>
+                );
+              })}
             </Div>
           </Box>
         </DropdownButton>
-        <Button variant="outline-secondary" id="button-addon2">
+        <Button
+          variant="outline-secondary"
+          id="button-addon2"
+          onClick={() => {
+            handleFalse();
+            handleValueNull();
+          }}
+        >
           검색
         </Button>
       </InputGroup>
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 995px;
+  height: 50px;
+  position: relative;
+  left: 50%;
+  margin-left: -497.5px;
+  margin-top: 170px;
+`;
 
 const Div = styled.div``;
 
