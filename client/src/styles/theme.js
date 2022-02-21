73c8b0893e817049b9ca31b15789e7dfc2ec2820
dongsu <sonicce99@naver.com>
2022-02-21 19:59:02 +0900

    style : theme color added

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -2,22 +2,14 @@
 //   mobile: "375px",
 // };
 
-// const colors = {
-//   colorBlue1: "#2557FF",
-//   colorBlue2: "#3178FF",
-//   colorLightGray1: "#B2B2B2",
-//   colorLightGray2: "#F7F8FA",
-//   colorLightGray3: "#F2F3F6",
-//   colorLightGray4: "#E3E7ED",
-//   colorGray1: "#737373",
-//   colorGray2: "#D6D8DC",
-//   colorGray3: "#888888",
-//   colorDarkGray1: "#2D2D2D",
-//   colorDarkGray2: "#333333",
-//   colorBlack: "#000000",
-//   colorPink: "#EA2E50",
-//   colorWhite: "#FFFFFF",
-// };
+const colors = {
+  colorBlue: "#4876EF",
+  colorSub: "#F4F6FA",
+  colorGray: "#8691A5",
+  colorLine: "#D8DCE4",
+  colorBlack: "#333333",
+  colorWhite: "#FFFFFF",
+};
 
 const fontWeights = {
   weightBold: "700",
@@ -37,7 +29,7 @@ const margins = {
 
 const theme = {
   // viewSize,
-  // colors,
+  colors,
   fontWeights,
   fontSize,
   margins,
