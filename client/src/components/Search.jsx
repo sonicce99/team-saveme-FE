25ebcf9e82f91bf855d8600f5544e0a7e0ab3422
dongsu <sonicce99@naver.com>
2022-02-22 09:16:25 +0900

    docs : style added

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../images/logo.png";
+import SearchBar from "./SearchBar";
+import SearchResult from "./SearchResult";
 
 const Search = () => {
-  return <h2>결과 보기</h2>;
+  return (
+    <Container>
+      <Div>
+        <Image src={logo} />
+        <SearchBar />
+      </Div>
+      <SearchResult />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 1200px;
+  margin: ${({ theme }) => theme.margins.marginCenter};
+`;
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 24px;
+`;
+
+const Image = styled.img`
+  width: 125px;
+  height: 24px;
+  margin-right: 50px;
+  cursor: pointer;
+`;
 
 export default Search;
